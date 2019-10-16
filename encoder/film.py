399d660c304287ab32a02b407f8ae28085c9230e
Daniel Ritter <danieldritter1@gmail.com>
2019-10-16 18:31:09 -0400

    Adding film layer

---
+++ encoder/film.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+class FiLM(tf.keras.layers.Layer):
+
+    def __init__(self):
+        super(FiLM,self).__init__()
+        self.lstm1 = tf.keras.layers.LSTM(2048)
+        self.d2 = tf.keras.layer.Dense(2)
+
+    def call(self,inputs):
+        # Maybe decide on specific activation functions here
+        out = self.lstm1(self.input,intial_state=self.lstm1.get_initial_state(inputs))
+        gamma, beta = self.d2(out)
+        return gamma, beta
