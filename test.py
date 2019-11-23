544f656f3711f65fb755276238689a918b372e80
sgsamuel <shamay.samuel@gmail.com>
2019-11-23 18:01:38 -0500

    Added basic 1v6 testing functionality

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ from diplomacy.utils.export import to_saved_game_format
 
 # importing from research
 from diplomacy_research.models import state_space
+from diplomacy_research.players.random_player import RandomPlayer
+from diplomacy_research.players.rule_based_player import RuleBasedPlayer
+from diplomacy_research.utils.cluster import start_io_loop, stop_io_loop
 
 # grabbing adjacency matrix
 adj_matrix = state_space.get_adjacency_matrix("standard")
@@ -54,3 +57,42 @@ while not game.is_game_done:
 print(reward_class.get_terminal_reward_all_powers())
 
 print(game.outcome)
+
+def main():
+    """ Plays a local game with 7 bots """
+    player1 = DipNetSLPlayer() # Use main player here x1
+    player2 = RandomPlayer() # Use other player here x6
+
+    game = Game()
+    reward_class = Reward(game)
+    supply_centers_dist = game.get_centers()
+
+    # For randomly choosing the power of the special player
+    powers = list(game.powers)
+    random.shuffle(powers)
+    powers1 = powers[0:1]
+    powers2 = powers[1:7]
+
+    # Playing game
+    while not game.is_game_done:
+        orders1 = yield {power_name: player1.get_orders(game, power_name) for power_name in powers1}
+        orders2 = yield {power_name: player2.get_orders(game, power_name) for power_name in powers2}
+
+        for power_name, power_orders in orders1.items():
+            game.set_orders(power_name, power_orders)
+        for power_name, power_orders in orders2.items():
+            game.set_orders(power_name, power_orders)
+        game.process()
+        print(reward_class.get_local_reward_all_powers())
+
+    print(reward_class.get_terminal_reward_all_powers())
+
+    print(game.outcome)
+
+    # Saving to disk
+    with open('game.json', 'w') as file:
+        file.write(json.dumps(to_saved_game_format(game)))
+    stop_io_loop()
+
+if __name__ == '__main__':
+    start_io_loop(main)
