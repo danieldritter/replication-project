8719308a706e298399f40c957913988d65af1e4b
dsam99 <dylanjiangsam@gmail.com>
2019-11-28 00:43:50 -0800

    adding to README and implementing chunking to train SL model at larger scale on GCP

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# replication-project
+# No-Press Diplomacy: Replication Project
+
+This repository holds our replication attempt of the [No-Press Diplomacy paper](https://arxiv.org/abs/1909.02128) for the NeuRIPS 2019 Reproducibility Challenge.  
--- SL_model.py
+++ SL_model.py
@@ -5,12 +5,51 @@ import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.optimizers import Adam
+import pickle
 
 from data import process
 from SL.encoder.encoder import Encoder
 from SL.decoder.decoder import Decoder
 from constants.constants import SEASON, UNIT_POWER, ORDER_DICT
 from AbstractActor import AbstractActor
+
+def set_sl_weights(new_weights, sl_model, state_inputs, prev_order_inputs, prev_orders_game_labels, season_names, board_dict_list):
+    # Finds winning power to use in network
+    last_turn = board_dict_list[i][-1]
+    prov_num_dict = defaultdict(int)
+    for province in last_turn:
+        if "unit_power" in last_turn[province].keys():
+            prov_num_dict[last_turn[province]["unit_power"]] += 1
+    power = max(prov_num_dict, key=prov_num_dict.get)
+
+    # Parsing just the season(not year)
+    # Not sure about these conversions
+    powers_seasons = []
+    curr_board_dict = board_dict_list[i]
+    # extracting seasons and powers for film
+    for j in range(len(season_names[i])):
+        # print(season_names[i][j][0])
+        powers_seasons.append(
+            SEASON[season_names[i][j][0]] + UNIT_POWER[power])
+    # print(powers_seasons)
+
+    # casting encoder and decoder inputs to floats
+    powers_seasons = tf.convert_to_tensor(powers_seasons,
+                                            dtype=tf.float32)
+    state_input = tf.convert_to_tensor(state_inputs[i],
+                                        dtype=tf.float32)
+    order_inputs = tf.convert_to_tensor(prev_order_inputs[i],
+                                        dtype=tf.float32)
+    season_input = season_names[i]
+
+    # applying SL model
+    orders_probs, position_lists = sl_model.call(state_input,
+                                             order_inputs,
+                                             powers_seasons,
+                                             season_input,
+                                             curr_board_dict,
+                                             power)
+    sl_model.set_weights(new_weights)
 
 class SL_model(AbstractActor):
     '''
@@ -94,7 +133,7 @@ class SL_model(AbstractActor):
                                                           season_input,
                                                           curr_board_dict,
                                                           power)
-                print(orders_probs.shape)
+                # print(orders_probs.shape)
                 if orders_probs.shape[0] != 0:
                     orders_probs = tf.transpose(orders_probs, perm=[2, 0, 3, 1])
                     orders_probs = tf.squeeze(orders_probs)
@@ -112,7 +151,28 @@ class SL_model(AbstractActor):
                                               self.trainable_variables)
                     self.optimizer.apply_gradients(
                         zip(gradients, self.trainable_variables))
+    
+    # def get_orders(self, game, power_names):
+        
+
+
 if __name__ == "__main__":
-    state_inputs, prev_order_inputs, prev_orders_game_labels, season_names, supply_center_owners, board_dict_list = process.get_data("data/standard_no_press.jsonl", num_games=1)
+    # initializing model with 16 layers of each as in original paper
     sl_model = SL_model(16, 16)
-    sl_model.train(state_inputs, prev_order_inputs, prev_orders_game_labels, season_names, board_dict_list)
+
+    # training SL_model in chunks of 
+    for i in range(1000):
+        state_inputs, prev_order_inputs, prev_orders_game_labels, season_names, supply_center_owners, board_dict_list = process.get_data("data/standard_no_press.jsonl", num_games=100)
+        sl_model.train(state_inputs, prev_order_inputs, prev_orders_game_labels, season_names, board_dict_list)
+            
+        # saving weights of SL model
+        weights_file = open("sl_weights.pickle","wb+")
+        pickle.dump(sl_model.get_weights(), weights_file)
+        weights_file.close()
+        print("Chunk %d" % (i))
+    
+    # setting weights of model
+    new_weights_file = open("sl_weights.pickle","rb+")
+    new_weights = pickle.load(new_weights_file)
+    new_sl_model = SL_model(16, 16)
+    set_sl_weights(new_weights, new_sl_model, state_inputs, prev_order_inputs, prev_orders_game_labels, season_names, board_dict_list)
